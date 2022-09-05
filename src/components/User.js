aecd3fe1254e7a838dc340949738823405455ac7
Fernando <fernando.mata.villalobos@gmail.com>
2022-09-05 14:50:42 -0600

    Switching User.js to component-based form.

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import classes from './User.module.css';
+import { Component } from "react";
+import classes from "./User.module.css";
 
 class User extends Component {
   render() {
